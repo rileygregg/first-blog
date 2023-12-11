a4e761ce6e3d2bce4c6ac3ad7061e2c7189979c6
Riley Gregg <rtgregg93@gmail.com>
2023-12-11 17:35:23 -0500

    add devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root "home#index"
+
+  get 'user/index'
+  get 'user/show'
+  get 'user/new'
+  get 'user/create'
+  get 'user/edit'
+  get 'user/update'
+  get 'user/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
